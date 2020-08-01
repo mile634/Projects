6691973f84ba59e0a5abfdf51a3e11a352ed0ebd
mile634 <mile634634@gmail.com>
2020-08-01 21:21:40 +0300

    DirectX initialized

--- DirectXGame/AppWindow.cpp
+++ DirectXGame/AppWindow.cpp
@@ -10,13 +10,18 @@ AppWindow::~AppWindow()
 
 void AppWindow::onCreate()
 {
+	//Window::onCreate();
+	GraphicsEngine::get()->init();
 }
 
 void AppWindow::onUpdate()
 {
+	//Window::onUpdate();
+
 }
 
 void AppWindow::onDestroy()
 {
 	Window::onDestroy();
+	GraphicsEngine::get()->release();
 }
